7c4b3679d9f02c0d4204cb9b5c6fa6b376e858ab
Raymond <mathsgod@yahoo.com>
2019-12-13 10:53:29 +0800

    update system logout

--- pages/System/logout.php
+++ pages/System/logout.php
@@ -6,18 +6,17 @@ class System_logout extends R\Page
 {
     public function get()
     {
-
-
+        
         if ($this->app->logined()) {
-            if ($_SESSION["app"]["org_user"]) {
-                $_SESSION["app"]["user"] = new App\User($_SESSION["app"]["org_user"]->user_id);
-                unset($_SESSION["app"]["org_user"]);
+            if ($_SESSION["app"]["org_user_id"]) {
+                $this->app->user = new App\User($_SESSION["app"]["org_user_id"]);
+                unset($_SESSION["app"]["org_user_id"]);
             }
 
             $this->app->user->offline();
+
             $user_id = $this->app->user->user_id;
             $w[] = "user_id=$user_id";
-
             if ($ul = UserLog::first($w, "userlog_id desc")) {
                 $ul->logout_dt = date("Y-m-d H:i:s");
                 $ul->save();
@@ -27,6 +26,6 @@ class System_logout extends R\Page
         $_SESSION["app"] = [];
 
         $pi = $this->app->pathinfo();
-        header("location: " . $pi["system_base"]);
+        header("location: " . $pi["cms_base"]);
     }
 }
