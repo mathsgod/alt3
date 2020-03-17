e85fa7153820251140db749a315de1bf4d4110cd
Raymond <mathsgod@yahoo.com>
2020-03-17 11:53:37 +0800

    date datetime example

--- pages/System/example/date.php
+++ pages/System/example/date.php
@@ -8,6 +8,8 @@ class System_example_date extends ALT\Page
         $e->add("Date YYYY-MM")->datetime("date2")->attr("format", "YYYY-MM");
         $e->add("Date minDate")->datetime("date3")->attr("min-date", "2019-06-01");
         $e->add("Date minDate and format")->datetime("date4")->attr("format", "YYYY-MM")->attr("min-date", "2019-06");
+        $e->add("Disabled date")->date("date1")->attr("disabled", true);
+        $e->add("Disabled datetime")->datetime("date1")->attr("disabled", true);
         $this->write($this->createForm($e));
     }
 }
