b0c85fb6d7b174a4d27c182a6e0f1a37e560326c
Raymond <mathsgod@yahoo.com>
2020-04-20 17:02:40 +0800

    update

--- src/plugin.js
+++ src/plugin.js
@@ -6,5 +6,4 @@ import "x-html-bs4/dist/x-html-bs4.umd.js";
 var Vue = window.Vue;
 import Element from 'element-ui';
 
-import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(Element, { size: "small" });
