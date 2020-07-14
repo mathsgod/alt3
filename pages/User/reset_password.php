465ebc62dc53f2b2be8659ac3a4f0371b1b89268
Raymond Chong <mathsgod@yahoo.com>
2020-07-14 17:16:13 +0800

    reset password

--- pages/User/reset_password.php
+++ pages/User/reset_password.php
@@ -3,11 +3,11 @@ class User_reset_password extends ALT\Page
 {
     public function post()
     {
-        if ($this->app->user->isAdmin()) {
-            $u = $this->object();
-        } else {
-            $u = $this->app->user;
+        if (!$this->app->user->isPowerUser() && !$this->app->user->isAdmin()) {
+            return $this->app->accessDeny($this->request);
         }
+
+        $u = $this->object();
         $u->password = password_hash($_POST["password"], PASSWORD_DEFAULT);
         $u->save();
         $this->alert->info("Password updated");
@@ -19,13 +19,8 @@ class User_reset_password extends ALT\Page
         $config = $this->app->config;
         $obj = $this->object();
 
-        $obj->password = "";
-        $mv = $this->createE($obj);
-        $mv->add("New password")->input("password")->type("password")->required()->attr("id", "password")
-            ->minLength($config["user"]["password-length"]);
-        $mv->add("Retype password")->input("password2")->type("password")->required();
-        $f = $this->createForm($mv);
-        $f->action();
-        $this->write($f);
+        if (!$this->app->user->isPowerUser() && !$this->app->user->isAdmin()) {
+            return $this->app->accessDeny($this->request);
+        }
     }
 }
