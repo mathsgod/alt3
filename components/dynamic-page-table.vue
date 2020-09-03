428fb1d94562e66d27f939208d5b898314012e53
Raymond Chong <mathsgod@yahoo.com>
2020-09-03 16:09:30 +0800

    dynamic table

--- components/dynamic-page-table.vue
+++ components/dynamic-page-table.vue
@@ -1,50 +1,58 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <td>
-          <el-button size="mini" type="primary" icon="fa fa-plus" @click="onClickAdd"></el-button>
-        </td>
-        <th v-for="(b,index) in structure" v-text="b.name" :key="index"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(d,i) in data" :key="i">
-        <td>
-          <el-button size="mini" type="danger" icon="fa fa-minus" @click="onClickDel(i)"></el-button>
-        </td>
-        <td v-for="(b,j) in structure" :key="j">
-          <el-input
-            v-model="d[b.name]"
-            v-if="b.type=='text'"
-            v-bind="b.attributes"
-            autosize
-            type="textarea"
-            show-word-limit
-            @input="$forceUpdate()"
-          ></el-input>
+  <div>
+    <div class="mb-3">
+      <el-button size="mini" type="primary" icon="fa fa-plus" @click="onClickAdd"></el-button>
+    </div>
+    <div
+      class="d-flex flex-wrap mb-3"
+      v-for="(d,i) in data"
+      :key="i"
+      style="background-color: #f3f3f3;padding: 15px;"
+    >
+      <div class="mr-5">
+        <el-button size="mini" type="danger" icon="fa fa-minus" @click="onClickDel(i)"></el-button>
+      </div>
+      <div class="flex-fill">
+        <div class="row">
+          <div
+            class="col-12 mb-3"
+            v-for="(b,j) in structure"
+            :key="j"
+            v-bind:class="{'col-lg-6 col-xl-4': b.type!='list'}"
+          >
+            <div v-text="structure[j].name"></div>
+            <el-input
+              v-model="d[b.name]"
+              v-if="b.type=='text'"
+              v-bind="b.attributes"
+              :autosize="{ minRows: 2}"
+              type="textarea"
+              show-word-limit
+              @input="$forceUpdate()"
+            ></el-input>
 
-          <input
-            v-if="b.type=='image'"
-            v-model="d[b.name]"
-            is="fileman"
-            :name="`s.name_${i}`"
-            :data-field="`s.name_${i}`"
-            url="Fileman/?token="
-          />
+            <input
+              v-if="b.type=='image'"
+              v-model="d[b.name]"
+              is="fileman"
+              :name="`s.name_${i}`"
+              :data-field="`s.name_${i}`"
+              url="Fileman/?token="
+            />
 
-          <div v-if="b.type=='list'">
-            <dynamic-page-table
-              ref="dpt"
-              :structure="b.body"
-              :data="d[b.name]"
-              @add="d[b.name].push($event)"
-            ></dynamic-page-table>
+            <div v-if="b.type=='list'">
+              <dynamic-page-table
+                ref="dpt"
+                :structure="b.body"
+                :data="d[b.name]"
+                @add="d[b.name].push($event)"
+              ></dynamic-page-table>
+            </div>
           </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 module.exports = {
