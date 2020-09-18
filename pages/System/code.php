7f35cd48f3939b649d3c09c100a94602a14a764d
Raymond Chong <mathsgod@yahoo.com>
2020-09-18 11:59:09 +0800

    update

--- pages/System/code.php
+++ pages/System/code.php
@@ -4,5 +4,7 @@ class System_code extends App\Page
 {
     public function get($source)
     {
+        $pi = $this->app->pathInfo();
+        $this->data["system_base"] = $pi["system_base"];
     }
 }
