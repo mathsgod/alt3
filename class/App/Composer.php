3a42b52df239a881760f7280c657d82e79164748
Raymond Chong <mathsgod@yahoo.com>
2020-09-29 13:19:18 +0800

    update

--- class/App/Composer.php
+++ class/App/Composer.php
@@ -10,79 +10,11 @@ class Composer
         $this->app = $app;
     }
 
-    public function auth(): array
-    {
-        return json_decode(file_get_contents($this->path() . "/auth.json"), true);
-    }
-
     public function installed(): array
     {
-        $ret = self::exec("show -f json");
-        $ret = json_decode($ret, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            //self update
-            self::exec("self-update");
-            $ret = self::exec("show -f json");
-            $ret = json_decode($ret, true);
-        }
-        return $ret["installed"] ?? [];
+        return $this->lockConfig()["packages"] ?? [];
     }
 
-    public function show()
-    {
-        return self::exec("show -f json");
-    }
-
-    public function package($name)
-    {
-        $path = $this->path();
-        foreach ($this->installed() as $p) {
-            if ($p["name"] == $name) {
-                $package = new Package();
-                $package->name = $p["name"];
-                $package->version = $p["version"];
-                $package->description = $p["description"];
-                $package->path = $path . "/vender/" . $name;
-
-                return $package;
-            }
-        }
-    }
-
-    public function getPHP(): string
-    {
-        $v = PHP_VERSION_ID;
-        $v = (string) intval($v / 100);
-        $v = $v[0] . intval(substr($v, 1));
-
-        $php = "php$v";
-        if (`$php -v`) {
-            return $php;
-        }
-        return "php";
-    }
-
-    public function exec(string $command): string
-    {
-        $cwd = getcwd();
-
-        putenv("COMPOSER_HOME=" . $this->path());
-
-        $phar = $this->phar();
-        chdir($this->path());
-
-        $php = $this->getPHP();
-        if ($command) {
-            $ret = `$php $phar $command 2>&1`;
-        } else {
-            $ret = `$php $phar 2>&1`;
-        }
-
-        chdir($cwd);
-
-        return $ret;
-    }
 
     public function hasPackage($package): bool
     {
@@ -100,43 +32,6 @@ class Composer
         return $p["composer_root"];
     }
 
-    public function packageSuggest($package)
-    {
-        $suggest = $this->exec("suggests $package");
-        $suggest = explode("\n", $suggest);
-        array_walk($suggest, "trim");
-        $suggest = array_filter($suggest, "strlen");
-        return $suggest;
-    }
-
-    public function suggests(): array
-    {
-        $suggest = $this->exec("suggests");
-        $suggest = explode("\n", $suggest);
-        array_walk($suggest, "trim");
-        $suggest = array_filter($suggest, "strlen");
-        return $suggest ?? [];
-    }
-
-    public function info(bool $checkupdate = false): array
-    {
-        if ($checkupdate) {
-            $info = $this->exec("show -l --format=json");
-        } else {
-            $info = $this->exec("show --format=json");
-        }
-
-        return json_decode($info, true)["installed"] ?? [];
-    }
-
-    public function config()
-    {
-        if (file_exists($file = $this->path() . "/composer.json")) {
-            return json_decode(file_get_contents($file), true);
-        }
-        return [];
-    }
-
     public function lockConfig()
     {
         if (file_exists($file = $this->path() . "/composer.lock")) {
@@ -145,44 +40,3 @@ class Composer
         return [];
     }
 
-    public function phar()
-    {
-        if (!file_exists($file = $this->path() . "/composer.phar")) {
-            file_put_contents($file, fopen("https://getcomposer.org/composer.phar", 'r'));
-        }
-        return $file;
-    }
-
-    public function changeOwn()
-    {
-        $folder = $this->path();
-        `find $folder -type d -exec chmod 0777 {} +`;
-        `find $folder -type f -exec chmod 0777 {} +`;
-    }
-
-    public function removeAll()
-    {
-        $folder = $this->path();
-        `rm -rf $folder`;
-    }
-
-    public function remove($package)
-    {
-        return $this->exec("remove $package");
-    }
-
-    public function install($package, $version)
-    {
-
-        $config = $this->config();
-
-        if ($config["require"][$package]) {
-            return;
-        }
-
-        $ret = $this->exec("require $package");
-        $this->changeOwn();
-
-        return $ret;
-    }
-}
--- pages/System/composer.php
+++ pages/System/composer.php
@@ -1,65 +1,15 @@
 <?php
-
-use App\Composer;
-
 class System_composer extends ALT\Page
 {
-    public function updateComposer()
+    public function get()
     {
-        $composer = new Composer($this->app);
-        $composer->exec("self-update");
-        return [true];
-    }
+        $t = $this->createT($this->app->composer()->installed());
+        $t->header->title = "Installed packages";
 
-    public function addRaymondRepo()
-    {
-        $composer = new Composer($this->app);
-        $composer->exec("config repositories.hostlink-raymond composer https://raymond2.hostlink.com.hk/satis/web");
-        return [true];
-    }
+        $t->add("Name", "name");
+        $t->add("Description", "description");
+        $t->add("Version", "version");
 
-    public function changeOwner()
-    {
-        $composer = new Composer($this->app);
-        $composer->changeOwn();
-        return [true];
-    }
-
-    public function run()
-    {
-        $composer = new Composer($this->app);
-        $ret = $composer->exec($_POST["cmd"]);
-
-        unlink($composer->path() . "/.htaccess");
-        $content = <<<EOL
-<Files ~ "\.(css|js|jpg|png|woff2|woff|ttf|html)$">
-Allow from all
-</Files>
-Deny from all
-EOL;
-        file_put_contents($composer->path() . "/.htaccess", $content);
-
-        return ["output" => $ret];
-    }
-
-    public function jsonFile()
-    {
-        $composer = new Composer($this->app);
-        $this->write("<pre>" . json_encode($composer->config(), 255) . "</pre>");
-    }
-
-    public function info()
-    {
-        $composer = new Composer($this->app);
-        return [
-            "installed" => $composer->installed(),
-            "suggests" => $composer->suggests()
-        ];
-    }
-
-    public function installedPackages()
-    {
-        $composer = new Composer($this->app);
-        return $composer->installed();
+        $this->write($t);
     }
 }
