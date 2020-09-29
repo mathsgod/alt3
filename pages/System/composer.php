8a45d1361ea0b8e29420879e07587f6ffad10591
Raymond Chong <mathsgod@yahoo.com>
2020-09-29 13:22:54 +0800

    update

--- pages/System/composer.php
+++ pages/System/composer.php
@@ -3,6 +3,8 @@ class System_composer extends ALT\Page
 {
     public function get()
     {
+        $this->callout->warning("Composer", "Install package thought apache are not supported, please use ssh to install package.");
+
         $t = $this->createT($this->app->composer()->installed());
         $t->header->title = "Installed packages";
 
