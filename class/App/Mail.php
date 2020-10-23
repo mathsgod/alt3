09ae3ee4e08c9d32e4945ee7e399bd973f51ebd4
Raymond Chong <mathsgod@yahoo.com>
2020-10-23 10:21:17 +0800

    maillog using core function

--- class/App/Mail.php
+++ class/App/Mail.php
@@ -4,24 +4,4 @@ namespace App;
 
 class Mail extends Core\Mail
 {
-    public function send()
-    {
-        foreach ($this->to as $to) {
-            $l = new MailLog();
-            $l->subject = $this->Subject;
-
-            $l->from = $this->From;
-            $l->from_name = $this->FromName;
-            $l->to = $to[0];
-            $l->to_name = $to[1];
-            $l->body = $this->Body;
-            $l->altbody = $this->AltBody;
-
-            $l->host = $this->Host;
-
-            $l->save();
-        }
-
-        return parent::send();
-    }
 }
