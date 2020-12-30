20bee56f62c628658a7825268efdbe2e189d1053
Raymond Chong <mathsgod@yahoo.com>
2020-12-30 16:47:41 +0800

    usergroup ae

--- pages/UserGroup/ae.php
+++ pages/UserGroup/ae.php
@@ -2,16 +2,12 @@
 
 class UserGroup_ae extends ALT\Page
 {
-
-
     public function get()
     {
-        $form = $this->createRForm([]);
+        $form = $this->createRForm();
         $form->add("Name")->input("name")->required();
         $form->add("Code")->input("code");
         $form->add("Remark")->textarea("remark");
-
-
         $this->write($form);
     }
 }
